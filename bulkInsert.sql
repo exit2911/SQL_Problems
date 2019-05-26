c465ace4778292c3868fa35245a409d99f4a3f2a
Vivian Nguyen <vyho0711@gmail.com>
2019-05-26 17:19:13 -0400

    Update bulkInsert.sql

--- bulkInsert.sql
+++ bulkInsert.sql
@@ -10,3 +10,20 @@ with
 rowterminator = '\n'
 
 )
+
+
+/*insert with delimiter*/
+
+
+bulk insert MusicDatabase.dbo.customers
+
+from 'C:\Users\vyho0\Downloads\customer.txt'
+
+with 
+
+(
+
+fieldterminator = ',', /*delimiter goes here*/
+
+rowterminator = '\n'
+
